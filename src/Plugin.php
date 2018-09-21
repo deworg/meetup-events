a38f353ebcda88317a039a7c2a8fecd305b7f0de
Florian Brinkmann <mail@flobri.de>
2018-09-21 14:11:02 +0200

    Add meta data

--- src/Plugin.php
+++ src/Plugin.php
@@ -22,6 +22,12 @@ class Plugin {
 	public function init() {
 		// Register the post type.
 		add_action( 'init', [ $this, 'register_post_type' ] );
+
+		// Register the post meta.
+		add_action( 'init', [ $this, 'register_post_meta' ], 900 );
+
+		// Add meta values to the REST API response.
+		add_filter( 'rest_prepare_meetup-events', [ $this, 'rest_prepare_meetup_events' ], 10, 2 );
 	}
 
 	/**
@@ -45,5 +51,55 @@ class Plugin {
 		];
 
 		register_post_type( 'meetup-events', $post_type_args );
+	}
+
+	/**
+	 * Register the needed post meta.
+	 */
+	public function register_post_meta() {
+		register_post_meta(
+			'meetup-events',
+			'meetup_events_date',
+			[
+				'type' => 'string',
+				'description' => esc_html__( 'The date of the meetup.', 'meetup-events' ),
+				'single' => true,
+				'show_in_rest' => true,
+			]
+		);
+
+		register_post_meta(
+			'meetup-events',
+			'meetup_events_time',
+			[
+				'type' => 'string',
+				'description' => esc_html__( 'Start time of the meetup.', 'meetup-events' ),
+				'single' => true,
+				'show_in_rest' => true,
+			]
+		);
+	}
+
+	/**
+	 * Add post meta data to REST API response.
+	 *
+	 * @param \WP_REST_Response $data Response data object.
+	 * @param \WP_Post $post Post object.
+	 *
+	 * @return \WP_REST_Response
+	 */
+	public function rest_prepare_meetup_events( $data, $post ) {
+		$date = get_post_meta( $post->ID, 'meetup_events_date', true );
+		$time = get_post_meta( $post->ID, 'meetup_events_time', true );
+
+		if ( $date ) {
+			$data->data['meetup_date'] = $date;
+		}
+
+		if ( $time ) {
+			$data->data['meetup_time'] = $time;
+		}
+
+		return $data;
 	}
 }
