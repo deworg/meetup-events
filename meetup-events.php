aae9057703a684890c4fde2de20c06471e1c173f
Florian Brinkmann <mail@flobri.de>
2018-09-21 11:53:43 +0200

    Main plugin file and Class file

---
+++ meetup-events.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Meetup events importer plugin.
+ *
+ * @package   deworg\MeetupEvents
+ * @license   GPL-3.0
+ *
+ * @wordpress-plugin
+ * Plugin Name: Meetup Events
+ * Description: Description.
+ * Version:     0.1.0
+ * Author:      Matthias Kittsteiner, Florian Brinkmann
+ * Author URI:  https://florianbrinkmann.com/en/
+ * License:     GPL v2 http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * Text Domain: lazy-loading-responsive-images
+ */
+namespace deworg\MeetupEvents;
+
+// Load Composer autoloader. From https://github.com/brightnucleus/jasper-client/blob/master/tests/bootstrap.php#L55-L59
+$autoloader = dirname( __FILE__ ) . '/vendor/autoload.php';
+if ( is_readable( $autoloader ) ) {
+	require_once $autoloader;
+}
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+// Create object.
+$plugin = new Plugin();
+// Init the plugin.
+$plugin->init();
---
+++ src/Plugin.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Florian
+ * Date: 21.09.2018
+ * Time: 11:52
+ */
+
+namespace deworg\MeetupEvents;
+
+
+class Plugin {
+	public function __construct() {
+	}
+
+	public function init() {
+
+	}
+}
