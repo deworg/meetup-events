67f23f753219d9bf0f6485000b20a9ec685516d0
Florian Brinkmann <mail@flobri.de>
2018-09-21 12:42:55 +0200

    Register custom post type

--- src/Plugin.php
+++ src/Plugin.php
@@ -1,19 +1,49 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: Florian
- * Date: 21.09.2018
- * Time: 11:52
+ * Main class file.
  */
 
 namespace deworg\MeetupEvents;
 
-
+/**
+ * Class Plugin
+ * @package deworg\MeetupEvents
+ */
 class Plugin {
+	/**
+	 * Plugin constructor.
+	 */
 	public function __construct() {
 	}
 
+	/**
+	 * Init function.
+	 */
 	public function init() {
+		// Register the post type.
+		add_action( 'init', [ $this, 'register_post_type' ] );
+	}
 
+	/**
+	 * Register the post type.
+	 */
+	public function register_post_type() {
+		$post_type_args = [
+			'labels'       => [
+				'name'          => esc_html__( 'Meetup events', 'meetup-events' ),
+				'singular_name' => esc_html__( 'Meetup event', 'meetup-events' ),
+				'menu_name'     => esc_html__( 'Meetup events', 'meetup-events' ),
+			],
+			'public'       => true,
+			'show_in_rest' => true,
+			'rewrite'      => [
+				'slug' => 'event',
+			],
+			'supports'     => [
+				'title',
+			],
+		];
+
+		register_post_type( 'meetup-events', $post_type_args );
 	}
 }
